0a87bf4e1b2e6e429bf5792b05e19124840a086f
BhoomiC <reachbhoomi252@gmail.com>
2017-07-25 09:57:44 +0530

    Annotation assignment

---
+++ Annotation/src/Annotation.java
@@ -0,0 +1,34 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by chhedab on 7/24/2017.
+ */
+enum Day{
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
+enum Month{
+    JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP,OCT, NOV, DEC
+}
+
+enum Year{
+  YEAR2015, YEAR2016, YEAR2017, YEAR2018, YEAR2019, YEAR2020
+}
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.PACKAGE)
+@interface Company{
+    String name();
+    String location();
+}
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.CONSTRUCTOR)
+@interface TimeStamp{
+    Day day();
+    Month month();
+    Year year();
+        }
+public class Annotation {
+}
